603e2aa044ab62587818fd9d5503da4072709ed0
RamelJohn <rjelecito@gmail.com>
2020-02-12 15:39:56 +0800

    redux add age and down age plus history and how to filter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	// const [age, setAge] = useState(21);
+
+	// const onAgeUp = () => {
+	// 	setAge(age + 1);
+	// };
+
+	// const onAgeDown = () => {
+	// 	setAge(age - 1);
+	// };
+
+	render() {
+		return (
+			<div className="App">
+				<div>
+					Age:<span>{this.props.age}</span>
+				</div>
+				<button onClick={this.props.onAgeUp}>Age Up</button>
+				<button onClick={this.props.onAgeDown}>Age Down</button>
+				<hr />
+
+				<div>History</div>
+				<div>
+					<ul >
+						{this.props.history.map(el => (
+							<li className='historyItem' key={el.id} onClick={() => this.props.onDelItem(el.id)}>{el.age}</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = state => {
+	return {
+		age: state.age,
+		history: state.history,
+	};
+};
+
+const mapDispatchToProps = dispatch => {
+	return {
+		onAgeUp: () => dispatch({ type: 'AGE_UP', value: 1 }),
+		onAgeDown: () => dispatch({ type: 'AGE_DOWN', value: 1 }),
+		onDelItem: (id) => dispatch({type: 'DEL_ITEM', key: id })
+	};
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
---
+++ src/store/reducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+	age: 21,
+	history: [],
+};
+
+const reducer = (state = initialState, action) => {
+	const newState = { ...state };
+
+	switch (action.type) {
+		case 'AGE_UP':
+			return {
+				...state,
+				age: state.age + action.value,
+				history: state.history.concat({ id: Math.random(), age: state.age + action.value }),
+			};
+		case 'AGE_DOWN':
+			return {
+				...state,
+				age: state.age - action.value,
+				history: state.history.concat({ id: Math.random(), age: state.age - action.value }),
+			};
+
+		case 'DEL_ITEM':
+			return {
+				...state,
+				history: state.history.filter(el => el.id !== action.key),
+			};
+	}
+	return newState;
+};
+
+export default reducer;
