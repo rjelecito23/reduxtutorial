9585400c77666ad496cb2b64236fe8569430f441
rjelecito23 <57887134+rjelecito23@users.noreply.github.com>
2020-02-13 14:28:32 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,16 +9,6 @@ import { createStore, applyMiddleware } from 'redux'
 import reducer from './store/reducers/reducer'
 import thunk from 'redux-thunk'
 
-// const logAction = (store) => {
-//     return next => {
-//         return action => {
-//             const result = next(action);
-//             console.log(`Caught in the middleware ${JSON.stringify(result)}`)
-//             return result;
-//         }
-//     }
-// }
-
 const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
